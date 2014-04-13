d445d9a3e1eedf21e0bc5077471fa3536c2253cd
Michael Toth <tofudrummer@gmail.com>
2014-04-13 10:21:04 -0400

    updated gmail api to return unread messages

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from twython import Twython
 import json
+import imaplib
+import re
 
 # Flask setup
 app = Flask(__name__)
@@ -17,6 +19,39 @@ APP_KEY = 'U2dIR7IRpydoxaFSUk0hM4ZBZ'
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 tweets = twitter.search(q='LVHack', count=100, result_type='recent')
 
+# GMAIL API Setup
+# Note every hour need to change oauth key
+class gmail:
+    def __init__(self):
+        self.IMAP_SERVER = 'imap.gmail.com'
+        self.IMAP_PORT = 993
+        self.M = None
+        self.mailboxes = None
+        self.response = None
+
+    def login(self, username, password):
+        self.M = imaplib.IMAP4_SSL(self.IMAP_SERVER, self.IMAP_PORT)
+        return_code, self.response = self.M.login(username, password)
+        return return_code
+
+    def logout(self):
+        self.M.logout()
+
+    def get_mailboxes(self):
+        return_code, self.response = self.M.list()
+        for item in self.response:
+            self.mailboxes.append(item.split()[-1])
+        return rc
+
+    def get_mail_count(self, folder='Inbox'):
+        return_code, count = self.M.select(folder)
+        return count[0]
+
+    def get_unread_count(self, folder='Inbox'):
+        return_code, message = self.M.status(folder, "(UNSEEN)")
+        unreadCount = re.search("UNSEEN (\d+)", message[0]).group(1)
+        return unreadCount
+
 # routing
 @app.route('/')
 def index():
@@ -26,9 +61,11 @@ def index():
 def twitter(query):
     return json.dumps(tweets)
 
-@app.route('/gmail')
-def gmail():
-    return 'Fuck gmail'
+@app.route('/gmail/unread')
+def gmailRequest():
+    g = gmail()
+    g.login(request.args.get('username'), request.args.get('password'))
+    return g.get_unread_count()
 
 @app.route('/oauth2callback')
 def callback():
