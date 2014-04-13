7725323929071269a578030b187ec6eff5a77581
Michael Toth <tofudrummer@gmail.com>
2014-04-13 00:59:13 -0400

    Added Twitter integration on the server

--- hello.py
+++ hello.py
@@ -1,11 +1,33 @@
-from flask import Flask
-from TwitterAPI import TwitterAPI
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+from twython import Twython
+import json
 
 app = Flask(__name__)
-api = TwitterAPI('U2dIR7IRpydoxaFSUk0hM4ZBZ','tb9CIw1yl9wiVXIPvuQ8VmOCpPCAoVfFAUOlM8y0DGp5Z5gU8Y','1493725711-ZY9aLfjDcd8n80Q02Sw4HY3Vle3xOvUnCV75JoC','DvUhSD7eikCpgR7RaY3Nzy63Qd4x3OSGBlCPAz0IxgHvI')
+app.config.from_object(__name__)
+
+APP_KEY = 'U2dIR7IRpydoxaFSUk0hM4ZBZ'
+APP_SECRET = 'tb9CIw1yl9wiVXIPvuQ8VmOCpPCAoVfFAUOlM8y0DGp5Z5gU8Y'
+
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
+
+APP_KEY = 'U2dIR7IRpydoxaFSUk0hM4ZBZ'
+
+twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+tweets = twitter.search(q='LVHack', count=100, result_type='recent')
+#print json.dumps(tweets)
+
 
 @app.route('/')
-def hello():
-    r = api.request('search/tweets', {'q':'pizza'})
-    for item in r.get_iterator():
-        return item
+def index():
+    return 'Hello World'
+
+@app.route('/twitter')
+def twitter():
+    return json.dumps(tweets)
+
+@app.route('/facebook')
+def gmail():
+    return 'Fuck gmail'
