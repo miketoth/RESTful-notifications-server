684b13de5bf97130e8ff9c7eee767c60a1f5c8fb
Michael Toth <tofudrummer@gmail.com>
2014-04-13 01:05:15 -0400

    Allows user to specify trending twitter topic

--- hello.py
+++ hello.py
@@ -3,31 +3,29 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from twython import Twython
 import json
 
+# Flask setup
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.debug=True
 
+# Twitter API Setup
 APP_KEY = 'U2dIR7IRpydoxaFSUk0hM4ZBZ'
 APP_SECRET = 'tb9CIw1yl9wiVXIPvuQ8VmOCpPCAoVfFAUOlM8y0DGp5Z5gU8Y'
-
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
-
 APP_KEY = 'U2dIR7IRpydoxaFSUk0hM4ZBZ'
-
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
-tweets = twitter.search(q='LVHack', count=100, result_type='recent')
-#print json.dumps(tweets)
-
-
+# routing
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'Hello World! Why are you here?'
 
-@app.route('/twitter')
-def twitter():
+@app.route('/twitter/<query>')
+def twitter(query):
+    tweets = twitter.search(q=query, count=100, result_type='recent')
     return json.dumps(tweets)
 
-@app.route('/facebook')
+@app.route('/gmail')
 def gmail():
     return 'Fuck gmail'
