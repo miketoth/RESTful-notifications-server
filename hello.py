38a8f2a27d5f27edeffb7e10b439e7780e511e05
Michael Toth <tofudrummer@gmail.com>
2014-04-12 22:38:41 -0400

    trying connect with twitter

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from TwitterAPI import TwitterAPI
 
 app = Flask(__name__)
+api = TwitterAPI('U2dIR7IRpydoxaFSUk0hM4ZBZ','tb9CIw1yl9wiVXIPvuQ8VmOCpPCAoVfFAUOlM8y0DGp5Z5gU8Y','1493725711-ZY9aLfjDcd8n80Q02Sw4HY3Vle3xOvUnCV75JoC','DvUhSD7eikCpgR7RaY3Nzy63Qd4x3OSGBlCPAz0IxgHvI')
 
 @app.route('/')
 def hello():
-    return 'Hello World'
+    r = api.request('search/tweets', {'q':'pizza'})
+    for item in r.get_iterator():
+        return item
